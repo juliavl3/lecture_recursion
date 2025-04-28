0812dcb5293659435391c032c42f730e18139615
Vlcanova Julia <257066@vutbr.cz>
2025-04-28 10:45:51 +0200

    fibonacci

--- fibo.py
+++ fibo.py
@@ -1,7 +1,20 @@
 
-def main():
-    pass
 
+def recursive_nth_fibo(n):
+    if n == 0:
+        return n
+    if n == 1:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
+    # sequence = [0,1]
+    # while n > 1:
+    #     sequence.append(recursive_nth_fibo(n)[n - 1] + recursive_nth_fibo(n)[n - 2])
+
+def main():
+    number = int(input("Zadej cislo: "))
+    print(recursive_nth_fibo(number))
 
 if __name__ == "__main__":
     main()
